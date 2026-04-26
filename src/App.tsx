e97174dfaaa284773b69d56aa86629f6af2d3ec4
Craftoonix <j.j.lelipaly@gmail.com>
2026-04-26 18:28:36 +0200

    Added a background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Board from "./Board"
 import Rain from "./Rain"
+import "./App.css"
 
 function App() {
-  return <div><Board /><Rain /></div>
+  return (
+    <div>
+      <div className="bg">
+        <Board />
+        <Rain />        
+      </div>
+    </div>
+  )
 }
+
 
 export default App
