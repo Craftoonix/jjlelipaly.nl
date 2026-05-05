a0cbdec3a545e40764e727e74b757a81a5452bf8
Craftoonix <j.j.lelipaly@gmail.com>
2026-05-05 23:43:06 +0200

    Added my resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import Board from "./Board"
 import Rain from "./Rain"
+import Resume from './Resume';
 import "./App.css"
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <Routes><Route path="/resume" element={<Resume/>}/> </Routes>
       <div className="bg">
         <Board />
         <Rain />        
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Resume.tsx
@@ -0,0 +1,6 @@
+
+function resume () {
+    return <iframe src="/resume.pdf" title="Resume" style={{width:'100%',height:'100vh',border:0}} />;
+}
+
+export default resume
