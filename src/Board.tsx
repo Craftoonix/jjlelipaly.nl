ff5089bbb7af69a7e2367f94a9ac67089a6ba56a
Craftoonix <j.j.lelipaly@gmail.com>
2026-05-11 19:26:32 +0200

    Added a title to return to homepage with rainbow text

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import "./Board.css"
 import Home from "./Home"
 import AboutMe from "./AboutMe"
@@ -6,10 +6,17 @@ import AboutMe from "./AboutMe"
 function Message(){
     return (
         <main className={"container"}>
+            <div className={"title"}>
+                <nav>
+                    <Link to ="/"><span className={"rainbow-text-animated"}>Joshua's Webpage</span></Link>
+                </nav>
+                
+            </div>  
             <div className={"board"}>
+           
                 <Routes>
                     <Route path="/" element={<Home/>}/> 
-                    <Route path="/aboutme" element={<AboutMe/>}/>
+                    <Route path="/projects" element={<AboutMe/>}/>
                 </Routes>
             </div>
         </main>
