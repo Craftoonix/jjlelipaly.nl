36b40d007498305acaa8d117b136cb99b08b14a0
Craftoonix <j.j.lelipaly@gmail.com>
2026-05-20 16:31:24 +0200

    updated resume page

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,6 +1,8 @@
+import ResumeFile from "./assets/resume.pdf"
+
 
 function resume () {
-    return <iframe src="/resume.pdf" title="Resume" style={{width:'100%',height:'100vh',border:0}} />;
+    return <iframe src={ResumeFile} title="Resume" style={{width:'100%',height:'100vh',border:0}} />;
 }
 
 export default resume
